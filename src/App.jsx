c58d105a0bc7f3efca48dae562e3eb9e7fc8e658
David Andersson <lejonmanen@gmail.com>
2026-04-28 13:15:27 +0200

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+const App = () => {
+
+	return (
+		<div className="app">
+			<header>
+				<img src="/favicon.png" />
+				<h1> Fire chat </h1>
+				
+			</header>
+			<main>
+				main
+				<div className="box">
+					<p> box 1 </p>
+				</div>
+				<div className="box user right">
+					<p> box 2 </p>
+				</div>
+
+				<button className="primary"> yolo 1 </button>
+				<button className="ghost"> yolo 2 </button>
+			</main>
+		</div>
+	)
+}
+export default App
