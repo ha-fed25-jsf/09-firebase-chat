3110d0430663b97032959a2d15cad3a602173a42
David Andersson <lejonmanen@gmail.com>
2026-04-29 09:45:08 +0200

    Sort messages

--- src/data/crud.js
+++ src/data/crud.js
@@ -2,23 +2,33 @@ import { getFirestore, collection, addDoc, getDocs, getDoc, doc, updateDoc, dele
 import { db } from './database.js'
 
 
+const msgCol = collection(db, 'messages')
+
 // Meddelandeobjekt: (id,) sender, text, time
 async function getMessages() {
-	const msgCol = collection(db, 'messages')
 	const msgSnapshot = await getDocs(msgCol)
 	const msgList = msgSnapshot.docs.map(doc => {
-		console.log('Dokument från Firestore: ', doc)
+		// console.log('Dokument från Firestore: ', doc)
 		const rawData = doc.data()
 		rawData.time = toDateTimeString(rawData.time)
 		rawData.id = doc.id
-		console.log('getMessages returnerar objekt: ', rawData)
+		// console.log('getMessages returnerar objekt: ', rawData)
 		return rawData
+	})
+	msgList.sort((m1, m2) => {
+		if( m1.time < m2.time ) {
+			return -1
+		} else if( m1.time > m2.time ) {
+			return 1
+		} else {
+			0
+		}
 	})
 	return msgList
 }
 
 function toDateTimeString(firestoreTimestamp) {
-		// Anta att 'firestoreTimestamp' är ditt Timestamp-objekt från Firestore
+	// Anta att 'firestoreTimestamp' är ditt Timestamp-objekt från Firestore
 	const dateObject = firestoreTimestamp.toDate();
 	//dateObject.toLocalDate??
 	const year = dateObject.getFullYear();
@@ -36,7 +46,6 @@ function toDateTimeString(firestoreTimestamp) {
 
 async function sendMessage(message) {
 // await addDoc(fruitsCol, { name })
-	const msgCol = collection(db, 'messages')
 	const docRef = await addDoc(msgCol, {
 		...message,
 		time: Timestamp.now()
